74e1bc63f90dcf4969b738b8065dba38ac46a966
Daniel Kaltenbaugh <d.a.kaltenbaugh@gmail.com>
2016-12-12 12:50:17 -0500

    Refactored all steps through get jobs.  Next is to save the jobs in the database and then make it a smart API query from last known id.

--- config/bullhorn.api.php
+++ config/bullhorn.api.php
@@ -6,16 +6,50 @@
  * Time: 10:22 AM
  * TODO-Dan Move this functionality to the vendor autoload file
  */
+//  Variable declarations
+$bh_auth_url  = 'https://auth.bullhornstaffing.com/oauth/authorize';        // Step one
+$bh_oauth_url = 'https://auth.bullhornstaffing.com/oauth/token';            // Step two
+$bh_login_url = 'https://rest.bullhornstaffing.com/rest-services/login';    // Step three
 
-//  Bullhorn API Production keys for API read access
-$bh_auth_array = array(
-    'client_id' => '4c9b0ca8-de30-4b11-9105-511006674c74',
-    'client_secret' => 'se5QdUlZ9qslXB4z9jwhMPlkGv4ytrq6',
-    'username' => 'alegiantapi',
-    'password' => 'PositivePressAsleep@26',
-    'redirect_uri' => 'http://alegiantservices.com/app/',
-    'response_type' => 'code',
-    'action' => 'Login'
-);
 // Bullhorn API url to call to request an authorization token:
-$bh_auth_url = 'https://auth.bullhornstaffing.com/oauth/authorize';
+$bh_client_id       = '4c9b0ca8-de30-4b11-9105-511006674c74';
+$bh_client_secret   = 'se5QdUlZ9qslXB4z9jwhMPlkGv4ytrq6';
+$bh_username        = 'alegiantapi';
+$bh_password        = 'PositivePressAsleep@26';
+$bh_redirect_uri    = 'http://alegiantservices.com/app/';
+    //  Alegiant Bullhorn API redirect url for all API responses using this client_id & client_secret
+
+//  Bullhorn API Production keys for API read access - used in index.php, step one
+$bh_auth_array = array(
+    'client_id'     => $bh_client_id,
+    'client_secret' => $bh_client_secret,
+    'username'      => $bh_username,
+    'password'      => $bh_password,
+    'redirect_uri'  => $bh_redirect_uri,
+    'response_type' => 'code',
+    'action'        => 'Login',
+);
+
+// Bullhorn API data for API OAuth Token request - used in step two, index.php
+$bh_oauth_array = array(
+    'grant_type'    => 'authorization_code',
+    'code'          => '', // Code value is added in step two of index.php after retrieved from the API
+    'client_id'     => $bh_client_id,
+    'client_secret' => $bh_client_secret,
+    'redirect_uri'  => $bh_redirect_uri,
+);
+
+// Bullhorn API data for API Rest Services Login - step three of index.php
+$bh_login_array = array(
+    'access_token'  => '', // Code value is added in step three of index.php after retrieved from API
+    'version'       => '*',
+);
+
+// Bullhorn Rest Services access for job listing retrieval - step four of index.php
+$bh_job_array = array(
+    'BhRestToken'   => '',  // Code value is added in step three of index.php after retrieved from API
+    'query'         => "isOpen:1 AND isDeleted:0 AND NOT status:archive",
+    'fields'        => "id,clientCorporation,clientContact,description",
+    'count'         => 20,
+    'start'         => 0,
+);
--- index.php
+++ index.php
@@ -1,29 +1,90 @@
-<?php
-    /**
-     * Created by Dijatek, LLC
-     * User: Daniel Kaltenbaugh <d.a.kaltenbaugh@gmail.com>
-     * Date: 12/11/2016
-     * Time: 6:57 PM
-     */
-    // Autoload vendor files (guzzlehttp as of 12.11.2016)
-    require __DIR__ . '/vendor/autoload.php';
-    require_once('./config/bullhorn.api.php');
-
-    $client = new GuzzleHttp\Client();
-    $res = $client->request('GET', $bh_auth_url, ['query' => $bh_auth_array]);
-    //  Can do a try to make sure that `$res->getStatusCode();` is 200 and if not, log the error.
-
-    //  Use json_decode to parse the response object which is in the form of
-    //      {"code":"{actual code}","client_id":"{actual client id}"} 12.11.2016 ~ DAK
-    $json = json_decode($res->getBody());  // The code is $json->code
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <title>Alegaint Job Update</title>
 </head>
 <body>
-    <h1>Alegiant Application</h1>
+<h1>Alegiant Application</h1>
+<div class="container">
+    <h2>Progress</h2>
+    <?php
+    /**
+     * Created by Dijatek, LLC
+     * User: Daniel Kaltenbaugh <d.a.kaltenbaugh@gmail.com>
+     * Date: 12/11/2016
+     * Time: 6:57 PM
+     * TODO-Dan Move most of this code into a class - function is ok for now
+     * TODO-Dan review comments and naming conventions for accuracy
+     */
+    // Autoload vendor files (guzzlehttp as of 12.11.2016)
+    require ('./vendor/autoload.php');
+    require_once('./config/bullhorn.api.php');
+
+    // Local variables
+    $client = new GuzzleHttp\Client();
+
+    /**
+     *  Step one - request authorization code from Bullhorn API using Alegiant credentials
+     *      Requires configuration settings in /config/bullhorn.api.php and guzzlehttp from
+     *      the vendor directory (also in composer).  Returns JSON with the desired code
+     *      accessed via $json->code;
+     */
+    $json = httpCall('GET', $bh_auth_url, $bh_auth_array, "Authentication code retrieved...<br/>");
+    //  Use json_decode to parse the response object which is in the form of
+    //      {"code":"{actual code}","client_id":"{actual client id}"} 12.11.2016 ~ DAK
+    //  Push the bh_auth_code into the bh_oauth_code array
+    $bh_oauth_array['code'] = $json->code;
+
+    /**
+     *  Step two - Get OAuth Token from Bullhorn
+     */
+    $json = httpCall('POST', $bh_oauth_url, $bh_oauth_array, "Oauth token retrieved...<br/>");
+        // Returns access_token, token_type & refresh_token
+    $bh_login_array['access_token'] = $json->access_token;
+    // TODO-Dan store refresh_token in session variable or database for use throughout
+    /**
+     *  Step three - Login to Bullhorn REST services
+     */
+    $json = httpCall('POST', $bh_login_url, $bh_login_array, "Login successful...<br/>");
+        // Returns access_token, rest_token & rest_url
+    $bh_job_array['BhRestToken'] = $json->BhRestToken;
+    $BhRestURL                   = $json->restUrl;
+    $BhRestURL                  .= '/search/JobOrder';
+
+    /**
+     *  Step four - get jobs and show them
+     */
+    $json = httpCall('GET', $BhRestURL, $bh_job_array, "Found some jobs!<br/>");
+    // Show jobs until I figure out how to put them in the database
+    foreach ($json->data as $key){
+        echo "-------------------------------<br/><pre>";
+        var_dump($key);
+        echo "</pre>";
+    }
+    /**
+     *  Guzzle it up function httpCall - more to follow but this helps a lot
+     *  TODO-Dan revisit echo from function - this may be a silent runner with a
+     *      redirect so output would not be good.  At least move it out to the
+     *      same level as the function call
+     */
+    function httpCall($method, $url, $call_array, $success_message) {
+        /** TODO-Dan fill this out in the correct format and make sure it is in the correct place
+         * @param
+         * @throws
+         * @return
+         */
+        $client = new GuzzleHttp\Client();
+        try {
+            $res = $client->request($method, $url, ['query' => $call_array]);
+            echo ($success_message);
+            return json_decode($res->getBody());
+        } Catch (Exception $e) {
+            error_log("API request error: $e");
+            return false;
+        }
+    }
+?>
+</div>
+
 </body>
 </html>
